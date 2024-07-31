c2876ae44ac0e2fc687f2f94209c4d961753fcee
Ben Sheldon [he/him] <bensheldon@gmail.com>
2024-07-31 12:46:55 -0700

    Fix require `version.rb` path (#6)

--- lib/activerecord-has_some_of_many.rb
+++ lib/activerecord-has_some_of_many.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'active_support/lazy_load_hooks'
-require_relative "version"
+require_relative "activerecord/has_some_of_many/version"
 
 ActiveSupport.on_load(:active_record) do
   require_relative "activerecord/has_some_of_many/associations"
